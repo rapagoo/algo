666a0810372b9f2cd231559f5d65587b94212520
Sungrok Hong <lilspicypepper@gmail.com>
2026-02-28 11:34:09 +0900

    BOJ 9012 7-day review

--- BOJ/BOJ_9012/solution.cpp
+++ BOJ/BOJ_9012/solution.cpp
@@ -4,27 +4,18 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	int T;
-	cin >> T;
-	while (T--) {
+	int N;
+	cin >> N;
+	while (N--) {
 		string s;
 		cin >> s;
 		int count{};
-		bool isError{};
 		for (char c : s) {
-			if (c == '(') {
-				count++;
-			}
-			else {
-				count--;
-				if (count < 0) {
-					isError = true;
-					break;
-				}
-			}
+			if (c == '(') count++;
+			else count--;
+			if (count < 0) break;
 		}
-		if (!isError && count == 0) cout << "YES\n";
+		if (count == 0) cout << "YES\n";
 		else cout << "NO\n";
 	}
 }
